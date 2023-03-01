8b7887d0302be254fb0c7f41f810153625b527ab
John <john.gangi@dialexa.com>
2023-03-01 15:09:27 -0500

    feat(v0): adds petFriendly flag

--- src/main/java/com/dialexa/hrsp/Room.java
+++ src/main/java/com/dialexa/hrsp/Room.java
@@ -9,22 +9,23 @@ public class Room {
     private String roomNumber;
    // private String nightOfAvailability;
    // private boolean availability;
-   // private boolean petFriendly;
+    private boolean petFriendly;
    // private List<Bed> beds;
    // private String localeId;
 
-    public Room(String id, String roomNumber){
+    public Room(String id, String roomNumber, boolean petFriendly){
         this.id=id;
         this.roomNumber=roomNumber;
+        this.petFriendly=petFriendly;
     }
 
     public static List<Room> rooms = Arrays.asList(
-        new Room("Presidents' Suite", "01"),
-        new Room("Ocean View Room 1", "02"),
-        new Room("Ocean View Room 2", "03"),
-        new Room("Harborside 2 Double", "04"),
-        new Room("Harborside 1 King", "05"),
-        new Room("Harborside 2 Queen", "06")
+        new Room("Presidents' Suite", "01", false),
+        new Room("Ocean View Room 1", "02", false),
+        new Room("Ocean View Room 2", "03", false),
+        new Room("Harborside 2 Double", "04", false),
+        new Room("Harborside 1 King", "05", true),
+        new Room("Harborside 2 Queen", "06", true)
     );
 
     public static Room getByRoomNumber(String k) {
